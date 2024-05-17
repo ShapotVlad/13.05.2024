679e5aac870c504646913d5ac697cc7b536c396a
Vladimir <vlnshapat@gmail.com>
2024-05-17 23:03:40 +0300

    добавили решение

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿//Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
 
 bool IsSumNumEven(int num)
 {
@@ -36,4 +38,42 @@ while (true)
         break;
     }
     nums = nums + " " + num;
+}
+
+Console.WriteLine();
+
+
+while (true)
+{
+    Console.Write("Введите целое число или 'q' для выхода: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    if (int.TryParse(input, out int number))
+    {
+        if (IsSumOfDigitsEven(number))
+        {
+            Console.WriteLine("Сумма цифр числа четна. Программа завершена.");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+    }
+}
+
+static bool IsSumOfDigitsEven(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum % 2 == 0;
 }
